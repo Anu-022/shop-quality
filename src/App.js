eefc3e60a49e5c58475ed15df05e5cf96c9facff
Anu-022 <102599053+Anu-022@users.noreply.github.com>
2023-02-01 06:20:20 +0100

    Revert "made the updates as requested and also worked on the cart component "

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import { ProductProvider } from './context/ProductContext';
-import ProductListing from './components/ProductListing';
-import AddProductsToCart from './components/AddProductsToCart';
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <ProductProvider>
-        <AddProductsToCart/>
-        <ProductListing />
-      </ProductProvider>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
